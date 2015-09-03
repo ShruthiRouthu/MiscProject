17e223fcc5b3fbcf078598f9a60f26419e18550a
Professional <Professional@10.30.17.56>
2015-09-03 11:08:23 -0500

    final commit

---
+++ src/java/Model/LetterCountingService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Professional
+ */
+public class LetterCountingService {
+    
+    public int getLetterCount(String word)
+    {
+        return word.length();
+    }
+    
+}
